a65328c26976b536ab9d5a2190cfe9a2bfd82b11
cordis-dev <darius@adroiti.com>
2024-07-02 14:50:25 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,10 +1,15 @@
-namespace ConsoleApp6
+namespace ConsoleApp
 {
-    internal class Program
+    internal class Program2
     {
-        static void Main(string[] args)
+        static void newMethod1(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var b1 = args.Any();
+        }
+
+        static void newMethod2(string[] args)
+        {
+            var b2 = args.Any();
         }
     }
 }
